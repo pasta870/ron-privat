ba61746e9f5d42a0b2ed78a2d0fab3d3a30e3b1e
pasta870 <96292419+pasta870@users.noreply.github.com>
2022-02-20 11:24:39 +0800

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -55,6 +55,13 @@ class GameOverSubstate extends MusicBeatSubstate
 		FlxG.camera.target = null;
 
 		bf.playAnim('firstDeath');
+                #if mobileC
+		addVirtualPad(NONE, A_B);
+		var camcontrol = new FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		_virtualpad.cameras = [camcontrol];	
+		#end
 	}
 
 	override function update(elapsed:Float)
